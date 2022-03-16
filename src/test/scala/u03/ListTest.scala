c5ddb4ac1669ff2013d81733cbdf477454634c54
Eddie Barzi <73700112+19eddie@users.noreply.github.com>
2022-03-16 18:01:44 +0100

    test Flat Map

--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -31,4 +31,8 @@ class ListTest:
     val lst = Cons(10, Cons(20, Cons(30, Nil())))
     val tail = Cons(40, Nil ())
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(lst, tail))
-    
+
+  @Test def testFlatMap() =
+    val lst = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(lst)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(lst)(v => Cons(v + 1, Cons(v + 2, Nil()))))
